daf73460dfc9f378c98592f67309c3cead337b4e
sting7401 <sting7401@naver.com>
2022-06-15 10:37:42 +0900

    type 다시

---
+++ test.js
@@ -0,0 +1,7 @@
+"use strict";
+let a;
+a = 'mark';
+a = 39;
+function hello(b) { }
+hello(39);
+hello('amdkf');
---
+++ test.ts
@@ -0,0 +1,10 @@
+let a: string | number;
+
+a = 'mark';
+
+a = 39;
+
+function hello(b: number) {}
+
+hello(39);
+hello('amdkf');
