06b1a487f55d4c0bb787b70c8c4cf5269b10d637
sting7401 <sting7401@naver.com>
2022-06-18 12:30:31 +0900

    symbol, null

---
+++ basic-types/null.js
@@ -0,0 +1,6 @@
+"use strict";
+// let myName: string = null;
+//let u: undefined = null;
+let v = undefined; //오로지 undefined 만 가능
+let union = null; // 합집합
+union = 'name';
---
+++ basic-types/null.ts
@@ -0,0 +1,9 @@
+// let myName: string = null;
+
+//let u: undefined = null;
+
+let v: void = undefined; //오로지 undefined 만 가능
+
+let union: string | null = null; // 합집합
+
+union = 'name';
