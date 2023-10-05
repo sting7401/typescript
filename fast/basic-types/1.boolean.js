b789d26dcb2beb8813a41cc4d00ec02c7f450266
KyeongHwanSong <sting7410@naver.com>
2023-10-05 21:30:29 +0900

    다시 시작

--- fast/basic-types/1.boolean.js
+++ fast/basic-types/1.boolean.js
@@ -1,1 +1,7 @@
 "use strict";
+let isDone = false;
+isDone = true;
+console.log(typeof isDone);
+let isOk = true;
+// 사용하지 않음
+//let isNotOk : boolean = new Boolean(false);
---
+++ fast/basic-types/1.boolean.ts
@@ -0,0 +1,10 @@
+let isDone:boolean = false;
+
+isDone = true;
+
+console.log(typeof isDone)
+
+let isOk : Boolean = true;
+
+// 사용하지 않음
+//let isNotOk : boolean = new Boolean(false);
